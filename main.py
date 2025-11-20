7736f938ad4a649b72ac0af0f33557cf4a9db9c1
lucas.tilly <lucas.tilly@epita.fr>
2025-11-20 19:24:45 +0100

    feat: make main transpile a decision tree classifier

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 import configparser
 from src.function_loader import FunctionLoader
-from src.dependencies import get_dependencies
-from src.metadata_retriever.metadata_retriever import MetadataRetriever
 import joblib
+from pathlib import Path
+
+from src.transpiler import Transpiler
 
 def main():
     conf = configparser.ConfigParser()
@@ -11,16 +12,12 @@ def main():
     function_loader = FunctionLoader(conf["FunctionLoader"])
 
     model = joblib.load("tests/tree.joblib")
-    metadata_retriever = MetadataRetriever(model=model, language="c")
-    metadata = metadata_retriever.retrieve_metadata()
-    print(function_loader["c"]["regular"]["includes"])
-    print("\n".join(metadata))
-    dependencies = get_dependencies("decision_tree")
-    for dep in dependencies:
-        print(function_loader["c"]["regular"][dep])
-    print(function_loader["c"]["regular"]["decision_tree"])
-    print(function_loader["c"]["main"]["decision_tree"])
-
+    transpiler = Transpiler(
+        model=model,
+        language="c",
+        output_file=Path("output.c"),
+    )
+    transpiler.transpile(function_loader=function_loader)
 
 if __name__ == "__main__":
     main()
