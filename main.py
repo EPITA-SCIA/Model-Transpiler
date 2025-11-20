e99282721a81d965e256e0ba5eb70d44405875c0
lucas.tilly <lucas.tilly@epita.fr>
2025-11-20 18:23:18 +0100

    feat: test metadata_retriever in main

--- main.py
+++ main.py
@@ -1,31 +1,29 @@
-from minitranspiler.minitranspiler import transpile
-from train_models import (
-    build_decision_tree_model,
-    build_linear_model,
-    build_binary_classes_logistic_model,
-    build_multi_classes_logistic_model,
-)
+import configparser
+from src.function_loader import FunctionLoader
+from src.dependencies import get_dependencies
+from src.metadata_retriever.metadata_retriever import MetadataRetriever
 import joblib
 
-build_linear_model()
-build_binary_classes_logistic_model()
-build_multi_classes_logistic_model()
-build_decision_tree_model()
+def main():
+    conf = configparser.ConfigParser()
+    conf.read(".config")
+
+    function_loader = FunctionLoader(conf["FunctionLoader"])
+
+    model = joblib.load("tests/tree.joblib")
+    metadata_retriever = MetadataRetriever(model=model, language="c")
+    metadata = metadata_retriever.retrieve_metadata()
+    print(metadata)
+    # dependencies = get_dependencies("logistic_regression")
+    # print(dependencies)
+    # hardcode values
+    # for dep in dependencies:
+    #     # Here you would do something with the loaded dependency
+    #     # For demonstration, we just print its content
+    #     print(function_loader["c"]["regular"][dep])
+    # print(function_loader["c"]["regular"]["logistic_regression"])
+    # print(function_loader["c"]["main"]["logistic_regression"])
 
 
-input = [205.9991686803, 2, 0]
-
-transpile(model=joblib.load("linear.joblib"), input=input, verbose=True)
-transpile(
-    model=joblib.load("logistic_binary.joblib"),
-    input=input,
-    verbose=True,
-    output="logistic_binary_model.c",
-)
-transpile(
-    model=joblib.load("logistic_multi.joblib"),
-    input=input,
-    verbose=True,
-    output="logistic_multi_model.c",
-)
-transpile(model=joblib.load("tree.joblib"), input=input, verbose=True)
+if __name__ == "__main__":
+    main()
