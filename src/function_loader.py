75d170e87e358c3813d4582b302b00c883877b24
lucas.tilly <lucas.tilly@epita.fr>
2025-11-20 18:22:34 +0100

    feat: make function loader handle mains

--- src/function_loader.py
+++ src/function_loader.py
@@ -4,21 +4,34 @@ from pathlib import Path
 class FunctionLoader:
     def __init__(self, config: dict):
         self.vendor_directory = Path(config["VENDOR_DIRECTORY"])
+
         self.map = self._get_function_map(self.vendor_directory)
 
     def _get_function_map(self, directory: Path) -> dict:
-        function_map = {"c": dict(), "verilog": dict()}
+        function_map = {"c": {"main": dict(), "regular": dict()}, "verilog": {"main": dict(), "regular": dict()}}
         c_dir = directory / "c"
         verilog_dir = directory / "verilog"
 
-        for file in c_dir.glob("*.c"):
+    
+        for file in c_dir.glob("main_functions/*.c"):
             function_name = file.stem
-            function_map["c"][function_name] = self._load_file(file)
+            function_map["c"]["main"][function_name] = self._load_file(file)
 
-        for file in verilog_dir.glob("*.v"):
+        for file in c_dir.glob("regular_functions/*.c"):
             function_name = file.stem
-            function_map["verilog"][function_name] = self._load_file(file)
+            function_map["c"]["regular"][function_name] = self._load_file(file)
+    
+        for file in verilog_dir.glob("main_functions/*.v"):
+            function_name = file.stem
+            function_map["verilog"]["main"][function_name] = self._load_file(file)
+
+        for file in verilog_dir.glob("regular_functions/*.v"):
+            function_name = file.stem
+            function_map["verilog"]["regular"][function_name] = self._load_file(file)
+        
         return function_map
+    
+
 
     def _load_file(self, path: Path) -> str:
         with open(path, "r") as file:
